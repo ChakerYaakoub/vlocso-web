cd053a373452faa34e1b922c296414038cdface8
ChakerYaakoub <117543062+ChakerYaakoub@users.noreply.github.com>
2024-11-23 15:33:02 +0100

    fix the eslint.config

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tseslint from '@typescript-eslint/eslint-plugin'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -24,7 +24,7 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
       // Disable the empty interface rule
-      '@typescript-eslint/no-empty-interface': true, // or configure 'allowInterfaces' option
+      '@typescript-eslint/no-empty-interface': 'warn',
       // Alternatively, use 'object' or 'unknown' as needed
       // 'your-rule-name': ['error', { allowInterfaces: true }],
     },
